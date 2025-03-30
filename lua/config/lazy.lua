32a3e9419bdd4b4a8828edbf3a96a5fd3e72a9f3
vike <rotkiv.imeinivik@gmail.com>
2025-03-30 22:51:23 +0300

    Added GitSigns

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -33,5 +33,5 @@ require("lazy").setup({
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-f>', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<C-g>', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.keymap.set('n', '<C-b>', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<C-h>', builtin.help_tags, { desc = 'Telescope help tags' })
